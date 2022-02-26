d5dae33912373388816e2d547249e78e60702758
patrick <patrick@hope.com>
2022-02-26 18:20:23 +0800

    zen mode: auto-hide word count, bring back title row

--- zenmode.js
+++ zenmode.js
@@ -6,3 +6,30 @@ api.addButtonToToolbar({
     },
     shortcut: 'alt+z'
 });
+
+$(document).ready(function(){
+    $(document).on({
+       keypress: debounce(function () { hideMore(); }, 500),
+       mousemove: debounce(function (e) { showSome(e); }, 500)
+    });
+});
+function hideMore() {
+    if (document.body.classList.contains('zen-mode'))
+        $("#center-pane > div:nth-child(2)").fadeTo("slow", 0); // word count
+}
+function showSome(e) {
+    var bottomY = $(window).height() - e.pageY;
+    if (bottomY <= 50)
+        $("#center-pane > div:nth-child(2)").fadeTo("slow", 1);
+}
+function debounce(func, interval) {
+    var lastCall = -1;
+    return function() {
+        clearTimeout(lastCall);
+        var args = arguments;
+        var self = this;
+        lastCall = setTimeout(function() {
+            func.apply(self, args);
+        }, interval);
+    };
+}
